fc0810a83706bae197158a464c62fffc100d4abc
aaron-lopez22 <aaron.a.lopez22@gmail.com>
2023-01-10 20:53:15 -0700

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       
+
+       Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int userNumber = -1;
+
+    while (userNumber != number)
+    {
+        Console.Write("What is the magic number? ");
+        userNumber = int.Parse(Console.ReadLine());
+        
+
+
+            if (userNumber > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userNumber < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it.");
+            }
     }
+
+        
+}
+
+
 }
