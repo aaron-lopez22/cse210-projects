741f091ab21ffe192c5ca91eb85560c835273ba1
aaron-lopez22 <aaron.a.lopez22@gmail.com>
2023-01-11 21:50:55 -0700

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+    while (userNumber != 0)
+    {
+        Console.Write("Enter Number ");
+        userNumber = int.Parse(Console.ReadLine());
+
+
+
+    if (userNumber != 0)
+
+        numbers.Add(userNumber);
+
+
     }
+
+
+foreach (float number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+float total = 0;
+
+foreach (float number in numbers)
+{
+    total += number;
+}
+float maximumNumber = numbers[0];
+
+foreach (float number in numbers)
+{
+    if (number > maximumNumber)
+    {
+        maximumNumber = number;
+    }
+}
+
+    Console.WriteLine($"The sum is: {total}");
+
+    float average = total / numbers.Count;
+    Console.WriteLine($"The average is {average}");
+
+    Console.WriteLine($"The largest number is {maximumNumber}");
+
+}
+
+
+
 }
